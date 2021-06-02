9ccc22c137f2a8bd19aedd17149ee57e58aec660
Christoph Franke <krito@krito.de>
2021-06-02 20:53:36 +0200

    completed first draft of webpage

---
+++ layouts/default.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <div class="social">
+      <a class="link" href="https://www.facebook.com/Pimamip" target="_blank">
+        <svg class="facebook" viewBox="0 30 448 450">
+          <use xlink:href="/facebook.svg#icon" />
+        </svg>
+      </a>
+      <a class="link" href="https://www.instagram.com/daspima/" target="_blank">
+        <img class="instagram hover" src="/instagram.svg">
+        <img class="instagram white" src="/instagram-white.svg">
+      </a>
+      <a class="link" href="mailto:justpima@gmail.com" target="_blank">
+        <svg class="email" viewBox="0 0 512 512">
+          <use xlink:href="/email.svg#icon" />
+        </svg>
+      </a>
+    </div>
+
+    <header class="header">
+      <div class="left">
+        <nuxt-link to="/">
+          <img class="pima" src="/IMG_0663_PNG.webp" />
+        </nuxt-link>
+      </div>
+      <div class="right">
+        <img class="name" src="/IMG_0665_PNG.webp" />
+        <img class="dots" src="/IMG_0666_PNG.webp" />
+      </div>
+    </header>
+
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #171717;
+  color: white;
+
+  font-family: 'Consola', monospace;
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: 'Consola';
+  src: url('/CONSOLA.TTF') format('truetype');
+}
+</style>
+
+<style lang="scss" scoped>
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  pointer-events: none;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  .left {
+    width: 400px;
+    pointer-events: auto;
+  }
+
+  .pima {
+    width: 100%;
+    -webkit-filter: grayscale(1) invert(1);
+    filter: grayscale(1) invert(1);
+  }
+
+  .right {
+    width: 400px;
+  }
+
+  .name {
+    display: block;
+    width: 100%;
+    -webkit-filter: grayscale(1) invert(1);
+    filter: grayscale(1) invert(1);
+  }
+
+  .dots {
+    width: 50%;
+    -webkit-filter: grayscale(1) invert(1);
+    filter: grayscale(1) invert(1);
+    transform: translate(100%, -50%);
+  }
+}
+
+.social {
+  margin-top: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .link {
+    margin: 0 4px;
+  }
+
+}
+
+.facebook {
+  height: 32px;
+  color: white;
+  a:hover & {
+    background-color: white;
+    background: linear-gradient(90deg, rgba(0,0,0,0) 0%, rgba(255,255,255,1) 10%, rgba(255,255,255,1) 90%, rgba(0,0,0,0) 100%);
+    color: #3b5998;
+  }
+}
+
+.instagram {
+  height: 40px;
+  width: auto;
+
+  &.hover {
+    display: none;
+  }
+
+  a:hover & {
+    display: none;
+    &.hover {
+      display: inline-block;
+    }
+  }
+}
+
+.email {
+  height: 36px;
+  padding: 0 5px;
+  margin: 0 -5px;
+  color: white;
+  a:hover & {
+    background-color: white;
+    color: black;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,5 @@
 <template>
   <div>
-    <div class="social">
-      <a class="link" href="https://www.facebook.com/Pimamip" target="_blank">
-        <svg class="facebook" viewBox="0 30 448 450">
-          <use xlink:href="/facebook.svg#icon" />
-        </svg>
-      </a>
-      <a class="link" href="https://www.instagram.com/daspima/" target="_blank">
-        <img class="instagram hover" src="/instagram.svg">
-        <img class="instagram white" src="/instagram-white.svg">
-      </a>
-    </div>
-
-    <header class="header">
-      <div class="left">
-        <img class="pima" src="/IMG_0663_PNG.webp" />
-      </div>
-      <div class="right">
-        <img class="name" src="/IMG_0665_PNG.webp" />
-        <img class="dots" src="/IMG_0666_PNG.webp" />
-      </div>
-    </header>
-
     <div class="main">
       <img class="picture" src="/53892d_efb7351168b34e73a09d86d2ace5a2a1_mv2_d_2000_1333_s_2.webp" />
       <img class="picture" src="/53892d_ca8fffb156274b008d115bb951952bb3_mv2_d_2000_3000_s_2.webp" />
@@ -39,6 +17,7 @@
         <img class="book-page" src="/53892d_01dfd7090f274b8aa970749652dd52a1_mv2.webp" />
         <div />
         <img class="book-page" src="/53892d_a8fa60892753494b8e092ebaed225a71_mv2.webp" />
+        <div />
       </div>
     </div>
 
@@ -51,6 +30,11 @@
         <p class="description">artist, author, photograph and yoga teacher, living in Berlin.</p>
       </div>
     </div>
+
+    <footer class="footer">
+      <nuxt-link to="/impressum">Impressum</nuxt-link>
+      <a href="mailto:mailto:justpima@gmail.com">Kontakt</a>
+    </footer>
   </div>
 </template>
 
@@ -60,93 +44,8 @@ export default {
 }
 </script>
 
-<style>
-body {
-  background-color: #171717;
-  color: white;
-}
-</style>
 
 <style lang="scss" scoped>
-.header {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  pointer-events: none;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-start;
-
-  .left {
-    width: 400px;
-  }
-
-  .pima {
-    width: 100%;
-    -webkit-filter: grayscale(1) invert(1);
-    filter: grayscale(1) invert(1);
-  }
-
-  .right {
-    width: 400px;
-  }
-
-  .name {
-    display: block;
-    width: 100%;
-    -webkit-filter: grayscale(1) invert(1);
-    filter: grayscale(1) invert(1);
-  }
-
-  .dots {
-    width: 50%;
-    -webkit-filter: grayscale(1) invert(1);
-    filter: grayscale(1) invert(1);
-    transform: translate(100%, -50%);
-  }
-}
-
-.social {
-  margin-top: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  .link {
-    margin: 0 4px;
-  }
-
-}
-
-.facebook {
-  height: 32px;
-  color: white;
-  a:hover & {
-    background-color: white;
-    background: linear-gradient(90deg, rgba(0,0,0,0) 0%, rgba(255,255,255,1) 10%, rgba(255,255,255,1) 90%, rgba(0,0,0,0) 100%);
-    color: #3b5998;
-  }
-}
-
-.instagram {
-  height: 40px;
-  width: auto;
-
-  &.hover {
-    display: none;
-  }
-
-  a:hover & {
-    display: none;
-    &.hover {
-      display: inline-block;
-    }
-  }
-}
-
-
 .main {
   margin-top: -225px;
 }
@@ -164,10 +63,22 @@ body {
   display: grid;
   grid-gap: 0;
   grid-template-columns: 1fr 1fr 1fr;
-}
 
-.book-page {
-  width: 100%
+  > * {
+    width: 100%;
+    border-bottom: 3px solid white;
+
+    &:nth-last-child(1),
+    &:nth-last-child(2),
+    &:nth-last-child(3) {
+      border-bottom: none;
+    }
+    &:nth-child(3n + 2) {
+      width: calc(100% - 6px);
+      border-right: 3px solid white;
+      border-left: 3px solid white;
+    }
+  }
 }
 
 .book-description {
@@ -177,21 +88,12 @@ body {
   flex-direction: column;
 }
 
-@font-face {
-  font-family: 'Consola';
-  src: url('/CONSOLA.TTF') format('truetype');
-}
-
 .book-title {
-  font-family: 'Consola', monospace;
-  font-weight: normal;
   text-align: center;
   font-size: 40px;
   margin: 0;
 }
 .book-subtitle {
-  font-family: 'Consola', monospace;
-  font-weight: normal;
   text-align: center;
   font-size: 24px;
   margin-top: 20px;
@@ -231,6 +133,20 @@ body {
     text-align: left;
     font-size: 24px;
     margin-top: 20px;
+  }
+}
+
+.footer {
+  margin: 100px 0;
+  text-align: center;
+  font-size: 20px;
+
+  a {
+    color: white;
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
   }
 }
 
