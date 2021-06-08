1a49dc409ebd9639e7c826d68fe78ef8e952148f
Christoph Franke <krito@krito.de>
2021-06-08 19:30:26 +0200

    added title and meta description

---
+++ nuxt.config.js
@@ -0,0 +1,9 @@
+export default {
+  head: {
+    title: 'Pima | Clara Haseloff',
+    meta: [{
+      name: 'description',
+      content: 'Pima | Clara Haseloff | Berlin | Kunst | Yoga | Fotografie',
+    }]
+  }
+}
